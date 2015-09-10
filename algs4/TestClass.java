3f910b109f38b26062ae08cde9da2e54732b2a74
Dmitri G. Brengauz <dmitri@momus.net>
2015-09-09 21:52:04 -0600

    Some sniggling problems

---
+++ algs4/TestClass.java
@@ -0,0 +1,16 @@
+import edu.princeton.cs.algs4.*;
+
+/*
+ *
+ */
+
+/** 
+ * 
+ * 
+ */
+
+class TestClass {
+    public static void main(String[] args) {
+      System.out.println((char) ('a' + 4));
+    }
+}
